9bf2903c343d63f366d294a9eea04384a9664ed5
MatusTabi <matustabi44@gmail.com>
2025-03-13 21:15:14 +0100

    refactor: update If struct and parsing logic to use Command type directly

--- src/models/if.go
+++ src/models/if.go
@@ -1,23 +1,22 @@
 package models
 
 import (
-	"github.com/pocketix/pocketix-go/src/interfaces"
 	"github.com/pocketix/pocketix-go/src/services"
 	"github.com/pocketix/pocketix-go/src/tree"
 )
 
 type If struct {
 	Id        string
-	Block     []interfaces.Command
+	Block     []Command
 	Arguments *tree.TreeNode
 }
 
 func (i *If) Execute() error {
 	services.Logger.Println("Executing if")
-	if result, err, _ := i.Arguments.Evaluate(); err != nil {
+	if result, err := i.Arguments.Evaluate(); err != nil {
 		services.Logger.Println("Error executing if arguments", err)
 	} else {
-		if result.(bool) {
+		if result {
 			services.Logger.Println("If is true, can execute body")
 		} else {
 			services.Logger.Println("If is false, skip body")
@@ -27,7 +26,14 @@ func (i *If) Execute() error {
 	return nil
 }
 
-// func (i *If) String() string {
-// 	services.Logger.Println("Printing If")
-// 	return fmt.Sprintf("If, Body: %v, Arguments: %v", i.Block, i.Arguments)
-// }
+func (i *If) GetId() string {
+	return i.Id
+}
+
+func (i *If) GetBody() []Command {
+	return i.Block
+}
+
+func (i *If) GetArguments() *tree.TreeNode {
+	return i.Arguments
+}
--- src/parser/blocks.go
+++ src/parser/blocks.go
@@ -1,24 +1,30 @@
 package parser
 
 import (
-	"github.com/pocketix/pocketix-go/src/interfaces"
+	"fmt"
+
 	"github.com/pocketix/pocketix-go/src/models"
 	"github.com/pocketix/pocketix-go/src/tree"
 )
 
-func ParseBlocks(block models.Block) (interfaces.Command, error) {
-	args, err := ParseArguments(block.Arguments[0])
+func ParseBlocks(block models.Block) (models.Command, error) {
+	if len(block.Arguments) == 0 {
+		return nil, fmt.Errorf("block %s is missing arguments", block.Id)
+	}
+
+	arg := block.Arguments[0]
+	args, err := ParseArguments(arg)
 	if err != nil {
 		return nil, err
 	}
 
-	var parsedCommands []interfaces.Command
-	for _, block := range block.Body {
-		cmd, err := ParseBlocks(block)
+	var parsedCommands []models.Command
+	for _, subBlock := range block.Body {
+		cmd, err := ParseBlocks(subBlock)
 		if err != nil {
 			return nil, err
 		}
 		parsedCommands = append(parsedCommands, cmd)
 	}
-	return models.CommandFactory(block.Id, parsedCommands, tree.InitTree(block.Arguments[0].Type, args))
+	return models.CommandFactory(block.Id, parsedCommands, tree.InitTree(arg.Type, args))
 }
