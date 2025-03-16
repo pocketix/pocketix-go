1a17952bc4f7279bea8a4773d5443e687cbd73bc
MatusTabi <matustabi44@gmail.com>
2025-03-16 14:17:58 +0100

    feat: add ParseVariables function for handling variable parsing

---
+++ src/parser/variables.go
@@ -0,0 +1,30 @@
+package parser
+
+import (
+	"encoding/json"
+
+	"github.com/pocketix/pocketix-go/src/models"
+	"github.com/pocketix/pocketix-go/src/services"
+)
+
+func ParseVariables(data json.RawMessage, variableStore *models.VariableStore) {
+	services.Logger.Println("Parsing variables", data)
+
+	var variables map[string]any
+
+	if err := json.Unmarshal(data, &variables); err != nil {
+		services.Logger.Println("Error parsing variables", err)
+	}
+
+	for varName, varData := range variables {
+		varType, varValue := varData.(map[string]any)["type"], varData.(map[string]any)["value"]
+
+		variableStore.AddVariable(models.Variable{
+			Name:  varName,
+			Type:  varType.(string),
+			Value: varValue,
+		})
+	}
+
+	services.Logger.Println("Variables parsed", variableStore.Variables)
+}
