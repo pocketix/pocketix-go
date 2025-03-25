882172ca55de16928f59508c17ae6ae80b120dc3
MatusTabi <matustabi44@gmail.com>
2025-03-25 21:30:16 +0100

    feat: repeat command now supports variables

--- src/commands/repeat.go
+++ src/commands/repeat.go
@@ -9,19 +9,29 @@ import (
 )
 
 type Repeat struct {
-	Id    string
-	Count int
-	Block []Command
+	Id        string
+	Count     any
+	CountType string
+	Block     []Command
 }
 
 func (r *Repeat) Execute(variableStore *models.VariableStore) (bool, error) {
 	services.Logger.Println("Executing repeat")
 
-	if r.Count < 0 {
+	count := r.Count
+	if r.CountType == "variable" {
+		variable, err := variableStore.GetVariable(count.(string))
+		if err != nil {
+			return false, err
+		}
+		count = variable.Value.(int)
+	}
+
+	if count.(int) < 0 {
 		return false, fmt.Errorf("count cannot be negative")
 	}
 
-	for range r.Count {
+	for range count.(int) {
 		if result, err := ExecuteCommands(r.Block, variableStore); err != nil {
 			return result, err
 		}
@@ -41,6 +51,10 @@ func (r *Repeat) GetArguments() *tree.TreeNode {
 	return nil
 }
 
-func (r *Repeat) GetCount() int {
+func (r *Repeat) GetCount() any {
 	return r.Count
+}
+
+func (r *Repeat) GetCountType() string {
+	return r.CountType
 }
