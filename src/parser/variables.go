322beb1bf145aad20ac42d584c92cacb2924f456
MatusTabi <matustabi44@gmail.com>
2025-03-27 20:59:11 +0100

    fix: expression variables are evaluated last

--- src/parser/variables.go
+++ src/parser/variables.go
@@ -14,6 +14,14 @@ func ParseVariables(data json.RawMessage, variableStore *models.VariableStore) {
 		services.Logger.Println("Error parsing variables", err)
 	}
 
+	type ExpressionVariable struct {
+		Name  string
+		Type  string
+		Value any
+	}
+
+	expressionVariables := make([]ExpressionVariable, 0)
+
 	for varName, varData := range variables {
 		varType, varValue := varData.(map[string]any)["type"], varData.(map[string]any)["value"]
 
@@ -21,10 +29,27 @@ func ParseVariables(data json.RawMessage, variableStore *models.VariableStore) {
 		// 	services.Logger.Println("Parsing list of variables", varList)
 		// }
 
+		if varType == "boolean_expression" {
+			expressionVariables = append(expressionVariables, ExpressionVariable{
+				Name:  varName,
+				Type:  varType.(string),
+				Value: varValue,
+			})
+			continue
+		}
+
 		variableStore.AddVariable(models.Variable{
 			Name:  varName,
 			Type:  varType.(string),
 			Value: models.InitTree(varType.(string), varType, varValue, variableStore),
+		})
+	}
+
+	for _, expressionVariable := range expressionVariables {
+		variableStore.AddVariable(models.Variable{
+			Name:  expressionVariable.Name,
+			Type:  expressionVariable.Type,
+			Value: models.InitTree(expressionVariable.Type, expressionVariable.Type, expressionVariable.Value, variableStore),
 		})
 	}
 }
