a712fcc8c82cdd3dd51019ace12bb3c4ae450bcc
MatusTabi <matustabi44@gmail.com>
2025-03-28 15:47:49 +0100

    test: add test for complex program

---
+++ tests/complex_programs_test.go
@@ -0,0 +1,24 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/pocketix/pocketix-go/src/models"
+	"github.com/pocketix/pocketix-go/src/parser"
+	"github.com/pocketix/pocketix-go/src/services"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExecuteWhileSetVar(t *testing.T) {
+	assert := assert.New(t)
+
+	data := services.OpenFile("../programs/complex/prog2.json")
+	variableStore := models.NewVariableStore()
+
+	_, err := parser.Parse(data, variableStore)
+	assert.Nil(err, "Error should be nil, but got: %v", err)
+
+	variable, err := variableStore.GetVariable("count")
+	assert.Nil(err, "Error should be nil, but got: %v", err)
+	assert.Equal(float64(5), variable.Value.Value, "Variable value should be 5, but got: %v", variable.Value.Value)
+}
