5e3525bb7d8219d95fc5ae2aa6543e8526746476
MatusTabi <matustabi44@gmail.com>
2025-03-30 18:55:58 +0200

    refactor: enhance ToBool function to return error

--- src/utils/bool_util.go
+++ src/utils/bool_util.go
@@ -1,18 +1,35 @@
 package utils
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
-func ToBool(value any) bool {
+// TODO: Return error if the value is not convertible to bool
+func ToBool(value any) (bool, error) {
 	switch v := value.(type) {
 	case int, int8, int16, int32, int64:
-		return reflect.ValueOf(v).Int() != 0
+		return reflect.ValueOf(v).Int() != 0, nil
 	case float32, float64:
-		return reflect.ValueOf(v).Float() != 0
+		return reflect.ValueOf(v).Float() != 0, nil
 	case string:
-		return v != ""
+		if v == "true" {
+			return true, nil
+		}
+		if v == "false" {
+			return false, nil
+		}
+		return false, fmt.Errorf("cannot convert string %s to bool", v)
 	case bool:
-		return v
+		return v, nil
 	default:
-		return false
+		return false, fmt.Errorf("cannot convert %T to bool", v)
 	}
+}
+
+func BoolToInt(value bool) int {
+	if value {
+		return 1
+	}
+	return 0
 }
