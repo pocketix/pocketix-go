0a6db08120a49d234b4967e5ebcf542e57e2bf9b
MatusTabi <matustabi44@gmail.com>
2025-03-31 10:14:50 +0200

    feat: add alert command

---
+++ src/commands/alert.go
@@ -0,0 +1,71 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/pocketix/pocketix-go/src/models"
+	"github.com/pocketix/pocketix-go/src/services"
+)
+
+type Alert struct {
+	Id           string
+	Method       string
+	Receiver     string
+	ReceiverType string
+	Message      string
+	MessageType  string
+}
+
+func (a *Alert) Execute(variableStore *models.VariableStore) (bool, error) {
+	services.Logger.Println("Executing alert")
+
+	if a.Method != "phone_number" && a.Method != "email" {
+		return false, fmt.Errorf("invalid alert method")
+	}
+
+	receiver := a.Receiver
+	message := a.Message
+
+	if a.ReceiverType == "variable" {
+		variable, err := variableStore.GetVariable(a.Receiver)
+		if err != nil {
+			return false, err
+		}
+		receiver = variable.Value.Value.(string)
+	}
+
+	if a.MessageType == "variable" {
+		variable, err := variableStore.GetVariable(a.Message)
+		if err != nil {
+			return false, err
+		}
+		message = variable.Value.Value.(string)
+	}
+
+	services.Logger.Println("Sending alert with method", a.Method, "to receiver", receiver, "with message", message)
+	return true, nil
+}
+
+func (a *Alert) GetId() string {
+	return a.Id
+}
+
+func (a *Alert) GetBody() []Command {
+	return nil
+}
+
+func (a *Alert) GetArguments() *models.TreeNode {
+	return nil
+}
+
+func (a *Alert) GetMethod() string {
+	return a.Method
+}
+
+func (a *Alert) GetReceiver() (string, string) {
+	return a.Receiver, a.ReceiverType
+}
+
+func (a *Alert) GetMessage() (string, string) {
+	return a.Message, a.MessageType
+}
--- src/commands/command_factory.go
+++ src/commands/command_factory.go
@@ -8,26 +8,78 @@ func CommandFactory(id string, blocks []Command, tree []*models.TreeNode) (Comma
 	switch id {
 	case "if":
 		if len(tree) == 0 {
-			return &If{Id: id, Block: blocks}, nil
+			return &If{
+				Id:    id,
+				Block: blocks,
+			}, nil
 		}
-		return &If{Id: id, Block: blocks, Arguments: tree[0]}, nil
+		return &If{
+			Id:        id,
+			Block:     blocks,
+			Arguments: tree[0],
+		}, nil
 	case "else":
-		return &Else{Id: id, Block: blocks}, nil
+		return &Else{
+			Id:    id,
+			Block: blocks,
+		}, nil
 	case "elseif":
-		return &ElseIf{Id: id, Block: blocks, Arguments: tree[0]}, nil
+		return &ElseIf{
+			Id:        id,
+			Block:     blocks,
+			Arguments: tree[0],
+		}, nil
 	case "while":
 		if len(tree) == 0 {
-			return &While{Id: id, Block: blocks}, nil
+			return &While{
+				Id:    id,
+				Block: blocks,
+			}, nil
 		}
-		return &While{Id: id, Block: blocks, Arguments: tree[0]}, nil
+		return &While{
+			Id:        id,
+			Block:     blocks,
+			Arguments: tree[0],
+		}, nil
 	case "setvar":
-		return &SetVariable{Id: id, LVal: tree[0].Value.(string), LValType: tree[0].Type, RVal: tree[1].Value, RValType: tree[1].Type}, nil
+		return &SetVariable{
+			Id:       id,
+			LVal:     tree[0].Value.(string),
+			LValType: tree[0].Type,
+			RVal:     tree[1].Value,
+			RValType: tree[1].Type,
+		}, nil
 	case "repeat":
-		return &Repeat{Id: id, Count: int(tree[0].Value.(float64)), CountType: tree[0].Type, Block: blocks}, nil
+		return &Repeat{
+			Id:        id,
+			Count:     int(tree[0].Value.(float64)),
+			CountType: tree[0].Type,
+			Block:     blocks,
+		}, nil
 	case "switch":
-		return &Switch{Id: id, Block: blocks, Selector: tree[0].Value, SelectorType: tree[0].Type}, nil
+		return &Switch{
+			Id:           id,
+			Block:        blocks,
+			Selector:     tree[0].Value,
+			SelectorType: tree[0].Type,
+		}, nil
 	case "case":
-		return &Case{Id: id, Block: blocks, Value: tree[0].Value, Type: tree[0].Type}, nil
+		return &Case{
+			Id:    id,
+			Block: blocks,
+			Value: tree[0].Value,
+			Type:  tree[0].Type,
+		}, nil
+	case "alert":
+		return &Alert{
+			Id:           id,
+			Method:       tree[0].Value.(string),
+			Receiver:     tree[1].Value.(string),
+			ReceiverType: tree[1].Type,
+			Message:      tree[2].Value.(string),
+			MessageType:  tree[2].Type,
+		}, nil
 	}
+
 	return nil, nil
 }
