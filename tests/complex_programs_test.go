c8c67c6ddb37de0cfeaf9cb07aa2aa0c2592e287
MatusTabi <matustabi44@gmail.com>
2025-04-08 14:54:21 +0200

    test: fix executing complex program

--- tests/complex_programs_test.go
+++ tests/complex_programs_test.go
@@ -15,8 +15,14 @@ func TestExecuteWhileSetVar(t *testing.T) {
 	data := services.OpenFile("../programs/complex/prog2.json")
 	variableStore := models.NewVariableStore()
 
-	_, err := parser.Parse(data, variableStore, nil)
+	commandsList, err := parser.Parse(data, variableStore, nil)
 	assert.Nil(err, "Error should be nil, but got: %v", err)
+	assert.NotNil(commandsList, "Commands list should not be nil")
+
+	for _, command := range commandsList {
+		_, err := command.Execute(variableStore, nil)
+		assert.Nil(err, "Error should be nil, but got: %v", err)
+	}
 
 	variable, err := variableStore.GetVariable("count")
 	assert.Nil(err, "Error should be nil, but got: %v", err)
