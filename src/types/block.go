f260a6433ba817a399c61a5e0aa2efe8e8e4a3f4
MatusTabi <matustabi44@gmail.com>
2025-05-06 11:13:57 +0200

    feat: extend Block and Device types with additional fields

--- src/types/block.go
+++ src/types/block.go
@@ -4,4 +4,10 @@ type Block struct {
 	Id        string     `json:"id"`
 	Body      []Block    `json:"block"`
 	Arguments []Argument `json:"arguments"`
+	Devices   []Device   `json:"devices"`
+}
+
+type Device struct {
+	ID     string `json:"deviceId"`
+	Values []any  `json:"values"`
 }
