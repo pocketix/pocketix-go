87212c745dcab46acda42151a21348a96721f79f
MatusTabi <matustabi44@gmail.com>
2025-05-09 19:22:57 +0200

    refactor: remove unused method

--- src/models/referencedValueStore.go
+++ src/models/referencedValueStore.go
@@ -21,38 +21,6 @@ func (rvStore *ReferencedValueStore) GetReferencedValues() map[string]Referenced
 	return rvStore.ReferencedValues
 }
 
-func (rvStore *ReferencedValueStore) SetValuesToReferenced(referencedValuesToUpdate []ReferenceValueResponseFromBackend) error {
-	for _, referencedValueToUpdate := range referencedValuesToUpdate {
-		referencedTarget := referencedValueToUpdate.DeviceID + "." + referencedValueToUpdate.SDType.SDParameters[0].ParameterDenotation
-		referencedValue, ok := rvStore.ReferencedValues[referencedTarget]
-		if !ok {
-			return fmt.Errorf("referenced value %s not found", referencedTarget)
-		}
-
-		latestSnapshot := getLatestSnapshot(referencedValueToUpdate.SDParameterSnapshots)
-		if latestSnapshot.String != nil {
-			referencedValue.Value = *latestSnapshot.String
-			referencedValue.Type = "string"
-		} else if latestSnapshot.Number != nil {
-			referencedValue.Value = *latestSnapshot.Number
-			referencedValue.Type = "number"
-		} else if latestSnapshot.Boolean != nil {
-			referencedValue.Value = *latestSnapshot.Boolean
-			referencedValue.Type = "boolean"
-		} else {
-			return fmt.Errorf("no valid value found in the latest snapshot")
-		}
-		referencedValue.DeviceID = referencedValueToUpdate.DeviceID
-		referencedValue.ParameterName = referencedValueToUpdate.SDType.SDParameters[0].ParameterDenotation
-		rvStore.ReferencedValues[referencedTarget] = referencedValue
-	}
-	return nil
-}
-
-func getLatestSnapshot(snapshots []SDParameterSnapshot) SDParameterSnapshot {
-	return snapshots[0]
-}
-
 func (rvStore *ReferencedValueStore) GetReferencedValueFromStore(referencedTarget string) (*ReferencedValue, error) {
 	referencedValue, ok := rvStore.ReferencedValues[referencedTarget]
 	if !ok {
